6bd9c93ab41add81b899a82d83b8948b07f3e0d6
Zach Bergson <zach.bergson@gmail.com>
2015-10-15 15:02:54 -0400

    multiple games

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 //::::::::::::::::::::::::::::Global stuff:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-var dealButton = document.getElementById('deal');
+var dealButton = document.createElement('button');
+var betForm = document.getElementById('wager');
 var container = document.getElementById('container')
 var playerStack;
 var dealerStack;
@@ -15,7 +16,10 @@ var playerStackValue;
 var dealerStackValue;
 var playerCardsStart;
 var dealerCardsStart;
-
+var amount = document.getElementById('amount');
+var startingPot = 1000;
+var playerWins;
+var playerLoses;
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //::::::::::::::::::::::::::::Create my deck & shuffle it::::::::::::::::::::::::::::::::::::::::::::::
@@ -50,59 +54,75 @@ var deck = function() {
     shuffle(cards);
 }
 
-deck();
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//::::::::::::::::::::::::::::::::::::::Deal cards:::::::::::::::::::::::::::::::::::::::::::::::::::::
+//::::::::::::::::::::::::::::::::::::::Make Bet & Deal cards::::::::::::::::::::::::::::::::::::::::::
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 //when user clicks deal, we need to pop() from cards array and stick it into a new array for player & dealer
-dealButton.onclick = function() {
-	var removeForPlayer1 = cards.pop();
-	var removeForPlayer2 = cards.pop();
-	playerStack = [removeForPlayer1, removeForPlayer2];
+betForm.onsubmit = function(event) {	
+	event.preventDefault();
+	pot.innerHTML = "You have $" + startingPot +  " Your current bet is $" + amount.value + ". Click deal to begin!";
+	betForm.setAttribute('style', 'display: none');
+	dealButton.setAttribute('id', 'deal');
+	dealButton.innerHTML = "Deal";
+	pot.appendChild(dealButton);
 
-	playerCardsStart = playerStack[0]['name'] + " " + playerStack[1]['name'];
-	playerCards.innerHTML = playerCardsStart;
+		dealButton.onclick = function() {
+			playerWins = "You now have $" + (startingPot + parseInt(amount.value)) + ".";
+			playerLoses = "You now have $" + (startingPot - parseInt(amount.value)) + ".";
 
-	var removeForDealer1 = cards.pop();
-	var removeForDealer2 = cards.pop();
-	dealerStack = [removeForDealer1, removeForDealer2];
-	dealerCardsStart = dealerStack[0]['name'];
-	dealerCards.innerHTML = dealerCardsStart;
-	dealerStackValue = dealerStack[0]['value'] + dealerStack[1]['value'];
+			deck();
+			var removeForPlayer1 = cards.pop();
+			var removeForPlayer2 = cards.pop();
+			playerStack = [removeForPlayer1, removeForPlayer2];
 
-	//remove dealButton and add hit & stand buttons
+			playerCardsStart = playerStack[0]['name'] + " " + playerStack[1]['name'];
+			playerCards.innerHTML = playerCardsStart;
 
-	container.removeChild(dealButton);
+			var removeForDealer1 = cards.pop();
+			var removeForDealer2 = cards.pop();
+			dealerStack = [removeForDealer1, removeForDealer2];
+			dealerCardsStart = dealerStack[0]['name'];
+			dealerCards.innerHTML = dealerCardsStart;
+			dealerStackValue = dealerStack[0]['value'] + dealerStack[1]['value'];
 
-	hitButton.setAttribute('class', 'hit');
-	hitButton.innerHTML = "hit";
-	pot.appendChild(hitButton);
+			//remove dealButton and add hit & stand buttons
+			pot.removeChild(dealButton);
+			pot.innerHTML = "You have $1,000. Your current bet is $" + amount.value + "."
+			
 
-	standButton.setAttribute('class', 'stand');
-	standButton.innerHTML = "stand";
-	pot.appendChild(standButton);
+			hitButton.setAttribute('class', 'hit');
+			hitButton.innerHTML = "hit";
+			pot.appendChild(hitButton);
 
-	playerStackValue = playerStack[0]['value'] + playerStack[1]['value'];
+			standButton.setAttribute('class', 'stand');
+			standButton.innerHTML = "stand";
+			pot.appendChild(standButton);
 
-	//check if playerhand is equal to 21. If it is, stop the game and player wins.
+			playerStackValue = playerStack[0]['value'] + playerStack[1]['value'];
 
-	if (playerStackValue === 21) {
-		alert("player wins!!");
-	}
+			//check if playerhand is equal to 21. If it is, stop the game and player wins.
 
-	if (dealerStackValue === 21) {
-		alert('dealer wins');
-	}
+			if (playerStackValue === 21) {
+				pot.innerHTML = playerWins;
+				console.log("player wins!!");
+				restart();
+			}
 
-	else {
-		chooseHitOrStand();
-	}
-	
+			if (dealerStackValue === 21) {
+				console.log('dealer wins');
+				pot.innerHTML = playerLoses;
+				restart();
+			}
+
+			else {
+				chooseHitOrStand();
+			}
+			
+		}
 }
-
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //::::::::::::::::::::::::::::::::::::::Choose to hit or stand:::::::::::::::::::::::::::::::::::::::::
@@ -110,6 +130,8 @@ dealButton.onclick = function() {
 
 var chooseHitOrStand = function() {
 	//need to make a for loop for all clicks on hitbutton. Will do tomorrow during class. refer to tic tac toe for reference.
+playerWins = "You now have $" + (startingPot + parseInt(amount.value)) + ".";
+playerLoses = "You now have $" + (startingPot - parseInt(amount.value)) + ".";
 
 	hitButton.onclick = function() {
 		var removeforPlayer3 = cards.pop();
@@ -121,22 +143,26 @@ var chooseHitOrStand = function() {
 		playerStackValue = playerStackValue +  playerStack[playerStack.length - 1]['value'];
 
 		if (playerStackValue > 21) {
-			alert('player busts!');
+			pot.innerHTML = playerLoses;
+			console.log('player busts!');
+			restart();
 		}
 
 		else if (playerStackValue === 21) {
-			alert('player wins');
+			pot.innerHTML = playerWins;
+			restart();
 		}
 
-		else {
-			chooseHitOrStand();
-		}
+		// else {
+		// 	chooseHitOrStand();
+		// }
 	}
 
 	standButton.onclick = function() {
 		console.log('hi');
 		if (playerStackValue > 21) {
-			alert('player busts!');
+			pot.innerHTML = playerLoses;
+			// alert('player busts!');
 		}
 
 		else if (dealerStackValue < 17) {
@@ -147,7 +173,9 @@ var chooseHitOrStand = function() {
 			dealerStackValue = dealerStackValue + dealerStack[2]['value'];
 
 			if (dealerStackValue > 21) {
-				alert('dealer busts');
+				pot.innerHTML = playerWins;
+				console.log('dealer busts');
+				restart();
 			}
 
 			else {
@@ -155,19 +183,22 @@ var chooseHitOrStand = function() {
 			}
 		}
 
-		else if (dealerStackValue > 17) {
+		else if (dealerStackValue >= 17) {
 			var removeForDealer3 = cards.pop();
 			dealerStack.push(removeForDealer3);
 			dealerCardsStart = dealerCardsStart + " " + dealerStack[1]['name'];
 			dealerCards.innerHTML = dealerCardsStart;
 			if (dealerStackValue === 21) {
-				alert('dealer wins');
+				pot.innerHTML = playerLoses;
+				console.log('dealer wins');
+				restart();
 			}
 
 			else {
 				checkForWin();
 			}
 		}
+
 	}	
 }
 
@@ -176,20 +207,48 @@ var chooseHitOrStand = function() {
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 var checkForWin = function() {
+playerWins = "You now have $" + (startingPot + parseInt(amount.value)) + ".";
+playerLoses = "You now have $" + (startingPot - parseInt(amount.value)) + ".";
 
 	if (playerStackValue === 21) {
-			console.log('player wins');
+			pot.innerHTML = playerWins;
+			restart();
 	}
 	else if ((21 - playerStackValue) > (21 - dealerStackValue)) {
-			alert('dealer wins!')
+			pot.innerHTML = playerLoses;
+			console.log('dealer wins!')
+			restart();
 	}
 
 	else if ((21 - playerStackValue) < (21 - dealerStackValue))  {
-			alert('player wins!');
+			pot.innerHTML = playerWins;
+			console.log('player wins!');
+			restart();
 	}
 
 	else {
-		alert('draw');
+		console.log('draw');
+		restart();
+	}
+
+}
+
+var restart = function() {
+
+	if (pot.innerHTML === playerWins) {
+		betForm.setAttribute('style', 'display: block');
+	}
+
+	else if ((startingPot - parseInt(amount.value)) > 0 ) {
+		betForm.setAttribute('style', 'display: block');
+	}
+
+	else if (!(pot.innerHTML === playerLoses) && !(pot.innerHTML === playerLoses)) {
+		betForm.setAttribute('style', 'display: block');
+	}
+
+	else {
+		alert("You're out of cash :(");
 	}
 }
 
